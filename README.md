f78d016d4b152ae1bc90acbd562380e1fd9edc21
SamLam140330 <lamlam0328@gmail.com>
2023-01-20 16:22:12 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Computer Vision And Pattern Recognition
+
+A course related to computer vision and pattern recognition
+
+Just for me as a reference
---
+++ lab1/main.py
@@ -0,0 +1,26 @@
+import cv2
+
+print("OpenCV version: " + cv2.__version__)
+
+RgbImage = cv2.imread('lab1data/ET_1.jpg')
+
+print("Showing normal version image")
+cv2.imshow('RgbImage', RgbImage)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+grayImage = cv2.cvtColor(RgbImage, cv2.COLOR_BGR2GRAY)
+print("Showing gray version image")
+cv2.imshow('grayImage', grayImage)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+smoothImage = cv2.blur(RgbImage, (5, 5))
+print("Showing blur version image")
+cv2.imshow('smoothImage', smoothImage)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+print("Saving the blur version image")
+directory = './smoothImage.jpg'
+cv2.imwrite(directory, smoothImage)
